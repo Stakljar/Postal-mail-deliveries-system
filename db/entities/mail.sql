e7b0e3c03fccb72b3404aeb820bf572cdfc3b855
Dražen Antunović <73203225+Entiox@users.noreply.github.com>
2023-04-26 15:54:54 +0200

    Update mail.sql

--- db/entities/mail.sql
+++ db/entities/mail.sql
@@ -1,6 +1,6 @@
 CREATE TABLE mail(
 	id CHAR(36),
-    type VARCHAR(7) DEFAULT "letter",
+    type VARCHAR(7) DEFAULT "letter" CHECK (type in ("letter", "package")),
     name VARCHAR(50),
     is_fragile BOOLEAN DEFAULT FALSE,
     
